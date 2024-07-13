a87be1fbb742dd27ba581192cae9fa5368eec976
Zubariel <138178621+itszubariel@users.noreply.github.com>
2024-07-13 22:39:22 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// navigation.js
+document.getElementById("Button1").addEventListener("click", function() {
+    // Set the relative URL to the page/file you want to navigate to
+    var url = "code.html";
+  
+    // Navigate to the URL
+    window.location.href = url;
+  });
+  
+  
