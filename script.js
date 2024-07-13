49a5e7de371897c0d736d27538a93f34bf7902fa
Zubariel <138178621+itszubariel@users.noreply.github.com>
2024-07-13 22:57:42 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // navigation.js
 document.getElementById("Button1").addEventListener("click", function() {
     // Set the relative URL to the page/file you want to navigate to
-    var url = "code.html";
+    var url = "suggestion.html";
   
     // Navigate to the URL
     window.location.href = url;
